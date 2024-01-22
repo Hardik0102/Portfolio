6fc36479c07e5d20a6687b0b4f3e60d7ea258717
Hardik0102 <hardik1104j@gmail.com>
2024-01-22 09:53:25 +0530

    added counter

--- index.js
+++ index.js
@@ -7,3 +7,18 @@ function hideSidebar(){
     const sidebar = document.querySelector('.sideBar')
 sidebar.style.display = 'none'
 }
+
+const targetCounts = [70, 19, 22, 34];
+const counters = [];
+
+targetCounts.forEach((targetCount, index) => {
+    let upto = 0;
+    const countElement = document.querySelectorAll('.counter')[index];
+
+    counters.push(setInterval(() => {
+        countElement.innerHTML = ++upto;
+        if (upto === targetCount) {
+            clearInterval(counters[index]);
+        }
+    }, 100));
+});
